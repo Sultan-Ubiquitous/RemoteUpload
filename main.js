289d54c49fda8776a2236463d3fdad7fe0247c47
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-03-07 14:59:31 +0530

    Checking Mock libraries to run test without using real credentials

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import * as dotenv from 'dotenv';
-import { IAMClient } from "@aws-sdk/client-iam";
+import { IAMClient, CreateUserCommand } from "@aws-sdk/client-iam";
+import { mockClient } from 'aws-sdk-client-mock';
 
 dotenv.config();
+
+const s3Mock = mockClient(IAMClient);
 
 client = new IAMClient({
     region: process.env.region,
@@ -10,3 +13,15 @@ client = new IAMClient({
         secretAccessKey: process.env.secretAccessKey,
     },
 });
+
+const userRequirements = {
+    UserName: "Ideally will take from user",
+    PermissionsBoundary: "Will add these too",
+    Tags: [
+        {
+            Key: "UserId",
+            Value: "123456789",
+        },
+    ]
+
+}
