758039ac1725fe426028deea22ebec0ca83ff619
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-03-15 14:47:58 +0530

    Applied default encryption to create bucket function also made a function to add encryption to existing buckets

--- s3bucketfunctions.js
+++ s3bucketfunctions.js
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-import { S3Client, CreateBucketCommand } from '@aws-sdk/client-s3';
+import { S3Client, CreateBucketCommand, ServerSideEncryption, PutBucketEncryptionCommand } from '@aws-sdk/client-s3';
 
 dotenv.config();
 
@@ -22,11 +22,47 @@ const createBucket = async (bucketName) => {
 }
 
 
+const setEncryptionToExistingBucket = async (bucketName) => {
+    try {
 
-const putIntoBucket = async() => {
-    
+        const s3Client = new S3Client({
+            region: process.env.region,
+            credentials:{
+                accessKeyId: process.env.accessKeyId,
+                secretAccessKey: process.env.secretAccessKey,
+            },
+        });
+
+        const encryptionConfig = {
+            ServerSideEncryptionConfiguration: {
+                Rules: [
+                    {
+                        ApplyServerSideEncryptionByDefault: {
+                            SSEAlgorithm: "AES256",
+                        },
+                    },
+                ],
+            },
+        }
+
+        await s3Client.send(new PutBucketEncryptionCommand({
+            Bucket: bucketName,
+            ServerSideEncryptionConfiguration: encryptionConfig.ServerSideEncryptionConfiguration,
+        }));
+        console.log(`Default Encryption Applied for the bucket ${bucketName}`);
+    } catch (error) {
+        console.log("Some Error occured ", error);
+        
+    }
 }
 
 
+// const putIntoBucket = async() => {
+    
+// }
 
-export {createBucket};
+
+
+// export {createBucket};
+const bucket = 'azims-first-test-bucket'
+setEncryptionToExistingBucket(bucket);
